8788d5bdf4401f46aac2aa1359f261f864c3de52
Michael Sweet <michaelsweet93@gmail.com>
2023-11-15 01:18:52 +0000

    add google tag manager

--- resources/views/components/layout_unauthed.blade.php
+++ resources/views/components/layout_unauthed.blade.php
@@ -1,5 +1,14 @@
 <html>
     <head>
+        <!-- Google tag (gtag.js) -->
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-Y3Z5NVC2FM"></script>
+        <script>
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', 'G-Y3Z5NVC2FM');
+        </script>
         <meta charset="UTF-8" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link href="/css/app_{{ Cookie::get('theme') ?? 'blue' }}.css" rel="stylesheet" id="theme-stylesheet">
