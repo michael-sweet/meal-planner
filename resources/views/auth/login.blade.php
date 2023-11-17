a6d20cf31fa05d20ad3574873de96b838c2bb745
Michael Sweet <michaelsweet93@gmail.com>
2023-11-17 02:06:17 +0000

    fix login page

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,6 +1,4 @@
-@extends('layout_unauthed')
-
-@section('content')
+<x-layout_unauthed>
 <div class="container">
     <div class="row justify-content-center">
         <div class="col-md-8">
@@ -70,4 +68,4 @@
         </div>
     </div>
 </div>
-@endsection
+</x-layout_unauthed>
